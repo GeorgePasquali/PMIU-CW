401b9966582fa6fe921df6e85331ae54405d5a94
gpasquali <george.pasquali@gmail.com>
2018-03-21 12:18:14 +0200

    Working version with disposables. Data binding to be implemented.

--- app/src/main/java/com/example/george/rainbowcloud/MainActivity.java
+++ app/src/main/java/com/example/george/rainbowcloud/MainActivity.java
@@ -36,7 +36,7 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity"; // for logging
+    private static final String TAG = "MainActivityLOOOOG"; // for logging
 
     private final CompositeDisposable disposables = new CompositeDisposable();
 
@@ -47,12 +47,14 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<String> mAdapter;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         mHelper = new TaskDbHelper(this);
 
-        mTaskListView = (ListView) findViewById(R.id.list_todo);
+        mTaskListView = findViewById(R.id.list_todo);
 
         this.updateUI();
 
@@ -75,21 +77,7 @@ public class MainActivity extends AppCompatActivity {
                         .setTitle("Add a new task")
                         .setMessage("What do you want to do next?")
                         .setView(taskEditText)
-                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                String task = String.valueOf(taskEditText.getText());
-                                SQLiteDatabase db = mHelper.getWritableDatabase();
-                                ContentValues values = new ContentValues();
-                                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
-                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
-                                        null,
-                                        values,
-                                        SQLiteDatabase.CONFLICT_REPLACE);
-                                db.close();
-                                updateUI();
-                            }
-                        })
+                        .setPositiveButton("Add", (dialog1, which) -> scheduleTaskCreation(taskEditText))
                         .setNegativeButton("Cancel", null)
                         .create();
                 dialog.show();
@@ -102,27 +90,87 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void scheduleTaskCreation(EditText currentTask){
+        disposables.add(createTask(currentTask)
+                // Run on a background thread
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<String>(){
+
+                    @Override public void onComplete() {
+                        Log.d(TAG, "onComplete()");
+                    }
+
+                    @Override public void onError(Throwable e) {
+                        Log.e(TAG, "onError()", e);
+                    }
+
+                    @Override public void onNext(String string) {
+
+                        updateUI();
+
+                        Log.d(TAG, "onNext(" + string + ")");
+                    }
+
+                }));
+    }
+
+    private Observable<String> createTask(EditText currentTask){
+        return Observable.defer(() -> {
+
+            String task = String.valueOf(currentTask.getText());
+
+            SQLiteDatabase db = mHelper.getWritableDatabase();
+
+            ContentValues values = new ContentValues();
+
+            values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+
+            db.insertWithOnConflict(
+                    TaskContract.TaskEntry.TABLE,
+                    null,
+                    values,
+                    SQLiteDatabase.CONFLICT_REPLACE
+            );
+
+            db.close();
+
+            return Observable.just(TaskContract.TaskEntry.COL_TASK_TITLE );
+        });
+    }
+
     private void updateUI() {
+
         ArrayList<String> taskList = new ArrayList<>();
+
         SQLiteDatabase db = mHelper.getReadableDatabase();
-        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+
+        Cursor cursor = db.query(
+                TaskContract.TaskEntry.TABLE,
                 new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
-                null, null, null, null, null);
-        while (cursor.moveToNext()) {
+                null, null, null, null, null
+        );
+        while (cursor.moveToNext()) { // Used to fill the taskList from the database.
             int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
             taskList.add(cursor.getString(idx));
         }
 
         if (mAdapter == null) {
+
             mAdapter = new ArrayAdapter<>(this,
                     R.layout.item_todo, // what view to use for the items
                     R.id.task_title, // where to put the String of data
                     taskList); // where to get all the data
+
             mTaskListView.setAdapter(mAdapter); // set it as the adapter of the ListView
+
+
         } else {
+
             mAdapter.clear();
             mAdapter.addAll(taskList);
             mAdapter.notifyDataSetChanged();
+
         }
 
         cursor.close();
@@ -131,69 +179,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void deleteTask(View view) {
         View parent = (View) view.getParent();
-        TextView taskTextView = (TextView) parent.findViewById(R.id.task_title);
+
+        TextView taskTextView = parent.findViewById(R.id.task_title);
+
         String task = String.valueOf(taskTextView.getText());
+
         SQLiteDatabase db = mHelper.getWritableDatabase();
+
         db.delete(TaskContract.TaskEntry.TABLE,
                 TaskContract.TaskEntry.COL_TASK_TITLE + " = ?",
                 new String[]{task});
+
         db.close();
+
         updateUI();
     }
 
 
 }
-
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    private static final String TAG = "RxAndroidSamples";
-//
-//    private final CompositeDisposable disposables = new CompositeDisposable();
-//
-//    @Override protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-//            @Override public void onClick(View v) {
-//                onRunSchedulerExampleButtonClicked();
-//            }
-//        });
-//    }
-//
-//    @Override protected void onDestroy() {
-//        super.onDestroy();
-//        disposables.clear();
-//    }
-//
-//    void onRunSchedulerExampleButtonClicked() {
-//        disposables.add(sampleObservable()
-//                // Run on a background thread
-//                .subscribeOn(Schedulers.io())
-//                // Be notified on the main thread
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribeWith(new DisposableObserver<String>() {
-//                    @Override public void onComplete() {
-//                        Log.d(TAG, "onComplete()");
-//                    }
-//
-//                    @Override public void onError(Throwable e) {
-//                        Log.e(TAG, "onError()", e);
-//                    }
-//
-//                    @Override public void onNext(String string) {
-//                        Log.d(TAG, "onNext(" + string + ")");
-//                    }
-//                }));
-//    }
-//
-//    static Observable<String> sampleObservable() {
-//        return Observable.defer(new Callable<ObservableSource<? extends String>>() {
-//            @Override public ObservableSource<? extends String> call() throws Exception {
-//                // Do some long running operation
-//                SystemClock.sleep(1000);
-//                return Observable.just("one", "two", "three", "four", "five");
-//            }
-//        });
-//    }
-//}
---
+++ app/src/main/java/com/example/george/rainbowcloud/Models/Task.java
@@ -0,0 +1,39 @@
+package com.example.george.rainbowcloud.Models;
+
+/**
+ * Created by George on 3/14/2018.
+ * Generic model (not using mvc pattern)
+ */
+
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+}
